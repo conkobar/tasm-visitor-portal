1174754db58fbc756647dbc3d1f4136444bf3c5d
conkobar <connorkleine@gmail.com>
2023-12-13 14:55:17 -0600

    main.js too busy

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import { getCollection } from "./firestoreFunctions.js";
-
-// test getCollection and addDocument
-getCollection("test")
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
